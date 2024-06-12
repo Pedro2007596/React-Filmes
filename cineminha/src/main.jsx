e8f4002c81e51045b4823ade90c9e6a0a5c2303f
Pedro2007596 <99182973+Pedro2007596@users.noreply.github.com>
2024-06-12 09:38:46 -0300

    Push

--- cineminha/src/main.jsx
+++ cineminha/src/main.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -9,7 +8,7 @@ import Sobre from './pages/Sobre.jsx'
 import Contato from './pages/Contatos.jsx'
 import PageNotFound from './pages/PageNotFound.jsx'
 import Home from './pages/Home.jsx'
-
+import DetailMovie from './pages/DetailMovie.jsx'
 
 
 const router = createBrowserRouter(
@@ -20,6 +19,7 @@ const router = createBrowserRouter(
       children: [
         {index: true, element: <Home/>},
         {path: "filmes", element: <Filmes/>},
+        {path: 'filmes/:id', element: <DetailMovie/>},
         {path: "sobre", element: <Sobre/>},
         {path: "contato", element: <Contato/>},
         {path: "*", element: <PageNotFound/>}
@@ -32,4 +32,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
-)
+);
